4c715d9d5791ac2ddcae43a1e79787e533367703
superman <duonga2qp@gmail.com>
2022-05-15 20:09:28 +0700

    nopbai

---
+++ [Bai_tap]_Xu_ly_ngoai_le_su_dung_doi_tuong_RangeError/index.ts
@@ -0,0 +1,24 @@
+function calculator(number1, number2, operator) {
+    let result;
+    if (operator == '+') {
+        result = number1 + number2;
+    } else if (operator == '-') {
+        result = number1 - number2;
+    } else if (operator == '*') {
+        result = number1 * number2;
+    } else if (operator == '/') {
+        result = number1 / number2;
+    }else{
+        throw new RangeError('the operator must be an+,-,*,/')
+    }
+    return result;
+}
+
+try
+{
+console.log(calculator(3, 9, '%'));}
+catch (e){
+    if(e instanceof RangeError){
+        console.log(e.message)
+    }
+}
